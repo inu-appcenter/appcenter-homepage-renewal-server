2db4a30f8f72d88e2e3d269510228dcfd9f72a54
Wonjeong Kim <38902021+NARUBROWN@users.noreply.github.com>
2023-08-31 01:17:20 +0900

    Fix: 인수가 하나만 필요한 컨트롤러 메소드에서는 @PathVariable을 사용하도록 변경했습니다., NotBlank와 NotEmpty가 혼용되던 문제를 해결했습니다.
    #9

--- src/main/java/home/inuappcenter/kr/appcenterhomepagerenewalserver/controller/GroupController.java
+++ src/main/java/home/inuappcenter/kr/appcenterhomepagerenewalserver/controller/GroupController.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @RestController
@@ -23,8 +22,8 @@ public class GroupController {
     private final GroupService groupService;
 
     @Operation(summary = "그룹 멤버 (1명) 조회", description = "조회할 group_id을 입력해주세요")
-    @GetMapping
-    public ResponseEntity<GroupResponseDto> getGroup(final @NotNull Long id) {
+    @GetMapping("/{id}")
+    public ResponseEntity<GroupResponseDto> getGroup(final @PathVariable("id") Long id) {
         log.info("사용자가 id: " + id + "을(를) 가진 Grop을 요청했습니다.");
         GroupResponseDto groupResponseDto = groupService.getGroup(id);
         return ResponseEntity.status(HttpStatus.OK).body(groupResponseDto);
@@ -45,8 +44,8 @@ public class GroupController {
     })
     @PostMapping
     public ResponseEntity<GroupResponseDto> assignGroup(final @RequestBody @Valid GroupRequestDto groupRequestDto,
-                                                        final @NotNull Long member_id,
-                                                        final @NotNull Long role_id) {
+                                                        final Long member_id,
+                                                        final Long role_id) {
         log.info("사용자가 Group을 저장하도록 요청했습니다.\n" +
                 "GroupRequestDto의 내용: "+ groupRequestDto.toString());
             GroupResponseDto groupResponseDto = groupService.assignGroup(member_id, role_id, groupRequestDto);
@@ -56,7 +55,7 @@ public class GroupController {
     @Operation(summary = "그룹 멤버 (1명) 수정", description = "수정할 Group id를 입력해주세요")
     @PatchMapping
     public ResponseEntity<GroupResponseDto> updateGroup(final @RequestBody @Valid GroupRequestDto groupRequestDto,
-                                                        final @NotNull Long id) {
+                                                        final Long id) {
         log.info("사용자가 id: "+ id + "을(를) 가진 Group을 수정하도록 요청했습니다.\n" +
                 "GroupRequestDto의 내용: "+ groupRequestDto.toString());
         GroupResponseDto groupResponseDto = groupService.updateGroup(groupRequestDto, id);
@@ -64,8 +63,8 @@ public class GroupController {
     }
 
     @Operation(summary = "그룹 멤버 (1명) 삭제", description = "삭제할 Group id를 입력해주세요")
-    @DeleteMapping
-    public ResponseEntity<String> deleteGroup(final @NotNull Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteGroup(final @PathVariable("id") Long id) {
         log.info("사용자가 id: " + id + "을(를) 가진 Group을 삭제하도록 요청했습니다.");
         String result = groupService.deleteGroup(id);
         return ResponseEntity.status(HttpStatus.OK).body(result);
