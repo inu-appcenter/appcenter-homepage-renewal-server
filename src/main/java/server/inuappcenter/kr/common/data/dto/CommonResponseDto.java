de6024ce1d744089158f103d958b10d66da3e6a9
Wonjeong Kim <38902021+NARUBROWN@users.noreply.github.com>
2023-09-27 23:53:25 +0900

    Fix: 불필요한 어노테이션 삭제
    #21

--- src/main/java/server/inuappcenter/kr/common/data/dto/CommonResponseDto.java
+++ src/main/java/server/inuappcenter/kr/common/data/dto/CommonResponseDto.java
@@ -3,9 +3,8 @@ package server.inuappcenter.kr.common.data.dto;
 import lombok.*;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CommonResponseDto {
-    private String msg;
+    private final String msg;
     public CommonResponseDto (String msg) {
         this.msg = msg;
     }
