7f7beb295b11c0ccc6967cd3da21ec3d5b3b7b81
Wonjeong Kim <codingshark@inu.ac.kr>
2024-05-17 14:10:02 +0900

    [Fix] Valid 관련 수정

--- src/main/java/server/inuappcenter/kr/data/dto/request/MemberRequestDto.java
+++ src/main/java/server/inuappcenter/kr/data/dto/request/MemberRequestDto.java
@@ -5,11 +5,7 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Null;
-import javax.validation.constraints.Pattern;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -32,39 +28,30 @@ public class MemberRequestDto {
             example = "https://...",
             description = "프로필 이미지 URL"
     )
-    @Null
-    @Pattern(regexp = "^https?://.*$", message = "profileImage의 URL 형식이 올바르지 않습니다.")
     private String profileImage;
 
     @Schema(
             example = "https://...",
             description = "블로그 URL"
     )
-    @Null
-    @Pattern(regexp = "^https?://.*$", message = "blogLink의 URL 형식이 올바르지 않습니다.")
     private String blogLink;
 
     @Schema(
             example = "test@inu.ac.kr",
             description = "이메일"
     )
-    @Email
     private String email;
 
     @Schema(
             example = "https://...",
             description = "Github 저장소 URL"
     )
-    @Null
-    @Pattern(regexp = "^https?://.*$", message = "gitRepositoryLink의 URL 형식이 올바르지 않습니다.")
     private String gitRepositoryLink;
 
     @Schema(
             example = "https://...",
             description = "Behance URL"
     )
-    @Null
-    @Pattern(regexp = "^https?://.*$", message = "Behance의 URL 형식이 올바르지 않습니다.")
     private String behanceLink;
 
     @Schema(
@@ -77,6 +64,7 @@ public class MemberRequestDto {
             example = "000000000",
             description = "학번"
     )
+    @NotBlank(message = "학번이 비어있을 수 없습니다.")
     private String studentNumber;
 
     @Schema(
