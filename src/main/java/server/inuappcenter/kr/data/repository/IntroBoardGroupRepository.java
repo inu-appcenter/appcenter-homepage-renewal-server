a3192dc34d647d2b5c000724f88d42b3d2868b3c
82everywin <109841880+82everywin@users.noreply.github.com>
2026-01-20 18:00:48 +0900

    feat: 프로젝트 글 - 스택/멤버 Repository 단계 추가

---
+++ src/main/java/server/inuappcenter/kr/data/repository/IntroBoardGroupRepository.java
@@ -0,0 +1,14 @@
+package server.inuappcenter.kr.data.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import server.inuappcenter.kr.data.domain.IntroBoardGroup;
+import server.inuappcenter.kr.data.domain.board.IntroBoard;
+
+import java.util.List;
+
+@Repository
+public interface IntroBoardGroupRepository extends JpaRepository<IntroBoardGroup, Long> {
+    List<IntroBoardGroup> findAllByIntroBoard(IntroBoard introBoard);
+    void deleteAllByIntroBoard(IntroBoard introBoard);
+}
